9103e9cabe4381d65e36612137ac7b8e1553c32b
Wind&Moon <63218554+yangyunfeng-cyber@users.noreply.github.com>
2024-07-16 22:02:01 +0800

    Create 1. 二分查找.py

---
+++ 1. 数组/1. 二分查找.py
@@ -0,0 +1,18 @@
+# leetcode 704  https://leetcode.cn/problems/binary-search/description/
+
+
+# 二分法,左闭右闭区间
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1  # 定义target在左闭右闭的区间里，[left, right]
+
+        while left <= right:
+            middle = left + (right - left) // 2
+            
+            if nums[middle] > target:
+                right = middle - 1  # target在左区间，所以[left, middle - 1]
+            elif nums[middle] < target:
+                left = middle + 1  # target在右区间，所以[middle + 1, right]
+            else:
+                return middle  # 数组中找到目标值，直接返回下标
+        return -1  # 未找到目标值
